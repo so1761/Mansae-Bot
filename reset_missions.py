687fa9ec0cd2945d15275ca722e2485b39d0b18b
so1761 <57207096+so1761@users.noreply.github.com>
2025-02-14 13:49:05 +0900

    보상수령 여부도 초기화하도록 변경

--- reset_missions.py
+++ reset_missions.py
@@ -21,6 +21,9 @@ if users:
         dice_ref.update({"주사위" : False})
         if daily_missions:
             update_data = {mission_name + "/완료": False for mission_name in daily_missions}
+            update_data2 = {mission_name + "/보상수령": False for mission_name in daily_missions}
             mission_ref.update(update_data)  # 모든 미션 초기화
+            mission_ref.update(update_data2)
+
 
 print(f"{date_str} 모든 사용자의 일일미션이 초기화되었습니다.")
