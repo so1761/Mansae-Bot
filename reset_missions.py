8cd7a64279644bc136dc3e81e57cc549965f2abe
so1761 <57207096+so1761@users.noreply.github.com>
2025-02-26 17:30:12 +0900

    reset_missions 수정

--- reset_missions.py
+++ reset_missions.py
@@ -4,6 +4,8 @@ from firebase_admin import db
 from firebase_admin import credentials
 from datetime import datetime
 
+load_dotenv()
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 cred = credentials.Certificate("mykey.json")
 firebase_admin.initialize_app(cred,{
@@ -15,6 +17,56 @@ date_str = now.strftime("[%Y-%m-%d]")  # 로그 날짜 추가
 
 cur_predict_seasonref = db.reference("승부예측/현재예측시즌") # 현재 진행중인 예측 시즌을 가져옴
 current_predict_season = cur_predict_seasonref.get()
+
+refname = db.reference(f"승부예측/예측시즌/{current_predict_season}/예측포인트")
+name_data = refname.get()
+
+dice_nums = []
+for nickname, point_data in name_data.items():
+    refdice = db.reference(f"승부예측/예측시즌/{current_predict_season}/예측포인트/{nickname}/주사위")
+    dice_nums.append((refdice.get(),nickname))
+
+max_dice_num = max(dice_nums, key=lambda x: x[0])[0]
+
+winners = [name for num, name in dice_nums if num == max_dice_num]
+
+if len(winners) == 1:
+    point_message = f"{', '.join([f'**{winner}**' for winner in winners])}에게 **{max_dice_num}**포인트 지급! 🎉"
+else:
+    point_message = f"**{winners[0]}**님에게 **{max_dice_num}**포인트 지급! 🎉"
+data = {
+    "content": "",
+    "embeds": [
+        {
+            "title": "🎯 주사위 정산",
+            "description": f"어제 굴린 주사위 중 가장 높은 숫자는 **{max_dice_num}**입니다!",
+            "color": 0x00ff00,  # 초록색
+            "fields": [
+                {
+                    "name": "결과",
+                    "value": point_message
+                }
+            ],
+            "footer": {
+                "text": "Dice Bot",
+            }
+        }
+    ]
+}
+
+# 포인트 지급
+for winner in winners:
+    ref = db.reference(f"승부예측/예측시즌/{current_predict_season}/예측포인트/{winner}/")
+    point_data = ref.get()
+    point = point_data.get("포인트")
+    ref.update({"포인트" : point + max_dice_num})
+
+response = requests.post(WEBHOOK_URL, json=data)
+
+if response.status_code == 204:
+    print("✅ Embed 메시지 전송 성공!")
+else:
+    print(f"❌ 메시지 전송 실패! 상태 코드: {response.status_code}")
 
 ref = db.reference(f"승부예측/예측시즌/{current_predict_season}/예측포인트")
 users = ref.get()
@@ -44,3 +96,18 @@ if today.weekday() == 6:
     solowatch_ref = db.reference(f"승부예측/예측시즌/{current_predict_season}")
     solowatch_ref.update({"혼자보기포인트" : 100})
     print(f"{date_str} 혼자보기 포인트가 100으로 초기화되었습니다.")
+    data = {
+    "content": "",
+    "embeds": [
+        {
+            "title": "포인트 초기화",
+            "description": "혼자보기 포인트가 100으로 초기화되었습니다!",
+            "color": 0x00ff00,  # 초록색
+            "footer": {
+                "text": "Mansae-Bot",
+            }
+        }
+    ]
+    }
+
+    response = requests.post(WEBHOOK_URL, json=data)
