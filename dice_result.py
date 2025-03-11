5a8c88fca326f2bbef7169f0e6d8d424abaf28b9
so1761 <57207096+so1761@users.noreply.github.com>
2025-03-11 07:49:16 +0000

    변동로그 불러오는 위치가 잘못된 오류 수정

--- dice_result.py
+++ dice_result.py
@@ -63,7 +63,7 @@ for winner in winners:
     current_datetime = datetime.now() # 데이터베이스에 남길 현재 시각 기록
     current_date = current_datetime.strftime("%Y-%m-%d")
     current_time = current_datetime.strftime("%H:%M:%S")
-    change_ref = db.reference(f"승부예측/예측시즌/{current_predict_season}/예측포인트변동로그/{current_date}/{current_time}/{interaction.user.name}")
+    change_ref = db.reference(f"승부예측/예측시즌/{current_predict_season}/예측포인트변동로그/{current_date}/{current_time}/{winner}")
     change_ref.update({
         "포인트": point + max_dice_num,
         "포인트 변동": max_dice_num,
