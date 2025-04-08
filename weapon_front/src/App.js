fafd754aa26e488898d85b9d9cd34223f5d97988
toe_kyung <xoehfdl8182@gmail.com>
2025-04-08 15:59:32 +0900

    Add weapon_front

---
+++ weapon_front/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "./pages/LoginPage"; // 경로는 실제 파일 위치에 맞춰야 해
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
